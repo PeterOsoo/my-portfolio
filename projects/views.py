9ae29624481f36ea9e116f447d14fa98097d85bd
Peter Osoo <posoo617@gmail.com>
2024-03-17 14:09:05 +0300

    ProjectListAPIView and home view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Project
+from .serializers import ProjectSerializer
 
 # Create your views here.
+
+
+class ProjectListAPIView(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+def home(request):
+    return render(request, 'base.html')
