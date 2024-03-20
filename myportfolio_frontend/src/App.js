5783fd7b02f7cc6ef659dca05b71240c8dafe964
Peter Osoo <posoo617@gmail.com>
2024-03-20 17:15:42 +0300

    display app - react router

--- myportfolio_frontend/src/App.js
+++ myportfolio_frontend/src/App.js
@@ -1,9 +1,21 @@
-import Container from "./components/Container"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import HomePage from "./pages/HomePage"
+import ProjectsPage from "./pages/ProjectsPage"
+import NoPage from "./pages/NoPage"
 
 const App = () => {
 	return (
 		<div>
-			<Container />
+			<Router>
+				<Routes>
+					<Route path="/" exact element={<HomePage />} />
+
+					<Route path="projects" element={<ProjectsPage />} />
+
+					<Route path="*" element={<NoPage />} />
+				</Routes>
+			</Router>
 		</div>
 	)
 }
